700cb20a0d4a1b2998ecb93c33d2fa4950f36918
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2014-04-07 23:28:12 +0200

    removed debug statements

---
+++ README.md
@@ -0,0 +1,2 @@
+This contains a sample node.js application for demonstrating the features of Cloud Foundry
+
--- web.js
+++ web.js
@@ -1,19 +1,5 @@
 // web.js
 
-String.format = function() {
-    var formatString = arguments[0];
-    
-    // start with the second argument (i = 1)
-    for (var i = 1; i < arguments.length; i++) {
-        // "gm" = RegEx options for Global search (more than one instance)
-        // and for Multiline search
-        var regEx = new RegExp("\\{" + (i - 1) + "\\}", "gm");
-        formatString = formatString.replace(regEx, arguments[i]);
-    }
-    
-    return formatString;
-}
-
 var express = require("express");
 var logfmt = require("logfmt");
 var os = require("os");
@@ -23,9 +9,6 @@ app.use(logfmt.requestLogger());
 app.use('/', express.static(__dirname + '/public'));
 
 var port = Number(process.env.VCAP_APP_PORT || 5000);
-var nics = os.networkInterfaces();
-console.log(os.hostname());
-console.log(nics);
 
 app.get('/status', function(req, res) {
   var result = {};
@@ -35,6 +18,7 @@ app.get('/status', function(req, res) {
   res.set('Content-Type', 'application/json');
   res.send(JSON.stringify(result));
 });
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
